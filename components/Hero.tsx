42e5ce94ec751c5b7f77de17cb7efbdd17a19d79
its-pablo <pablobooks67@gmail.com>
2022-03-02 17:50:37 -0800

    SAME THING OMG

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -84,7 +84,7 @@ const Hero = () => {
                         title="Demo video showcasing Supabase"
                         className="absolute w-full h-full rounded-b-md"
                         style={{ top: 0, left: 0 }}
-                        onclick="this.muted = false"
+                        onClick="this.muted = false"
                       >
                         <source 
                           src="videos/open99Intro.mp4"
